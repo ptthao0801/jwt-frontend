d125b014d7ac1e977a4f22166704aeb1068127cb
ptthao0801 <ptthao0801@gmail.com>
2024-08-29 03:29:41 +0700

    mobile frontend design

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,26 +2,31 @@ import './Login.scss';
 
 const Login = (props) => {
     return (
-        <div className="login-container mt-3">
+        <div className="login-container">
             <div className="container">
                 <div className="row">
-                    <div className="content-left col-md-7">
+                    <div className="content-left content-left col-12 d-none col-md-7 d-md-block">
                         <div className='logo'>
-                            Facebook
+                            <img className='fb-logo img-fluid' src='https://static.xx.fbcdn.net/rsrc.php/y1/r/4lCu2zih0ca.svg'/>
                         </div>
-                        <div className='detail'>
+                        <div className='detail fs-3'>
                             Facebook helps you connect and share with the people in your life.
                         </div>
                     </div>
-                    <div className="content-right col-md-5 d-flex flex-column gap-3 py-3">
+                    <div className='logo-mobile d-md-none'>
+                            <img className='fb-logo-mobile img-fluid' src='https://static.xx.fbcdn.net/rsrc.php/y1/r/4lCu2zih0ca.svg'/>
+                        </div>
+                    <div className="content-right col-12 col-md-4 d-flex flex-column gap-3 py-3">
+
                         <input type='text' className='form-control' placeholder='Email address or phone number'/>
                         <input type='password' className='form-control' placeholder='Password'/>
                         <button className='btn btn-primary fw-bold'>Log in</button>
-                        <span className='text-center'>Forgotten password?</span>
+                        <span className='text-center'>
+                            <a className='forgotten-password' href='#'>Forgotten password?</a>
+                            </span>
                         <hr/>
                         <div className='text-center'>
-                            <button className='btn btn-success fw-bold'>Create new account</button>
-
+                            <button className='create-btn btn fw-bold'>Create new account</button>
                         </div>
 
                     </div>
