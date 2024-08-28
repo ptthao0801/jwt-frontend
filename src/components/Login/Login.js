3b75df41a449de57fa71fa2b226b49f2bf088690
ptthao0801 <ptthao0801@gmail.com>
2024-08-28 21:13:24 +0700

    build login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,33 @@
+import './Login.scss';
+
 const Login = (props) => {
     return (
-        <div className="login-container">
-            login
+        <div className="login-container mt-3">
+            <div className="container">
+                <div className="row">
+                    <div className="content-left col-md-7">
+                        <div className='logo'>
+                            Facebook
+                        </div>
+                        <div className='detail'>
+                            Facebook helps you connect and share with the people in your life.
+                        </div>
+                    </div>
+                    <div className="content-right col-md-5 d-flex flex-column gap-3 py-3">
+                        <input type='text' className='form-control' placeholder='Email address or phone number'/>
+                        <input type='password' className='form-control' placeholder='Password'/>
+                        <button className='btn btn-primary fw-bold'>Log in</button>
+                        <span className='text-center'>Forgotten password?</span>
+                        <hr/>
+                        <div className='text-center'>
+                            <button className='btn btn-success fw-bold'>Create new account</button>
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
         </div>
     )
 }
